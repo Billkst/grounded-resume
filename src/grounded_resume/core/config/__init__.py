9de1b45fdf6445f752e2212016aadc236c7d4c25
Billkst <2685949814@qq.com>
2026-04-30 14:33:25 +0800

    feat: add safety rule tables

---
+++ src/grounded_resume/core/config/__init__.py
@@ -0,0 +1,17 @@
+from .safety_rules import (
+    DEGREE_DOWNGRADE_TABLE,
+    REDLINE_PATTERNS,
+    ROLE_LIMITS,
+    VERB_DOWNGRADE_TABLE,
+    detect_unsupported_number,
+    get_downgraded_verb,
+)
+
+__all__ = [
+    "DEGREE_DOWNGRADE_TABLE",
+    "REDLINE_PATTERNS",
+    "ROLE_LIMITS",
+    "VERB_DOWNGRADE_TABLE",
+    "detect_unsupported_number",
+    "get_downgraded_verb",
+]
